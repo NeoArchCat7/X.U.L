508364f5c990aa27f125f58554ef1a9d0dbe569f
Cane7 <nace.malezic@gmail.com>
2024-06-27 21:32:56 +0200

    Base Code

---
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,10 @@
+# 1 "C:\\Users\\Gregor\\Desktop\\Nace\\Programiranje\\VS Code\\GasperMIDI\\GasperMIDI.ino"
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+ Serial.println("Hello!");
+}
---
+++ build/sketch/GasperMIDI.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\Gregor\\Desktop\\Nace\\Programiranje\\VS Code\\GasperMIDI\\GasperMIDI.ino"
+#line 1 "C:\\Users\\Gregor\\Desktop\\Nace\\Programiranje\\VS Code\\GasperMIDI\\GasperMIDI.ino"
+void setup();
+#line 6 "C:\\Users\\Gregor\\Desktop\\Nace\\Programiranje\\VS Code\\GasperMIDI\\GasperMIDI.ino"
+void loop();
+#line 1 "C:\\Users\\Gregor\\Desktop\\Nace\\Programiranje\\VS Code\\GasperMIDI\\GasperMIDI.ino"
+void setup()
+{
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	Serial.println("Hello!");
+}
+
